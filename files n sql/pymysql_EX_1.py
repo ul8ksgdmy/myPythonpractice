db0b0a87d7c152032879a84b2e75e5deb745a239
my <clickmy7@gmail.com>
2018-10-10 22:24:44 +0900

    .

--- files n sql/pymysql_EX_1.py
+++ files n sql/pymysql_EX_1.py
@@ -22,12 +22,10 @@ password = data['passwd']
 db = data['database']
 
 # MySQL Connection 연결
-conn = pymysql.connect(host=host, user=user, password=password,
-                       db=db, charset='utf8')
+conn = pymysql.connect(host=host, user=user, password=password,db=db, charset='utf8')
  
 # Connection 으로부터 Dictoionary Cursor 생성
-# curs = conn.cursor(pymysql.cursors.DictCursor)
-curs = conn.cursor()
+curs = conn.cursor(pymysql.cursors.DictCursor)
  
 # SQL문 실행
 sql = "select * from customers where id=%s and address=%s"
@@ -36,7 +34,14 @@ curs.execute(sql, (9, 'one way 98'))
 # 데이타 Fetch
 rows = curs.fetchall()
 for row in rows:
+    #dictionary형태로 데이터 출력 만약 dictionary cursor가 아니었으면 튜플 출력
+    # 출력비교
+    # conn.cursor(pymysql.cursors.DictCursor)의 경우
+    # {'name': 'Susan', 'address': 'One way 98', 'id': 9}
+    # conn.cursor()의 경우
+    # ('Susan', 'One way 98', 9)
     print(row)
+    #dictionary형태이기 때문에 아래와 같은 print가 가능
     print(row['id'], row['name'], row['address'])
  
 # Connection 닫기
