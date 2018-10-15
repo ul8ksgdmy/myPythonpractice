32e7708df32ca781eae163c44fabd7bf7d567865
ul8ksgdmy <clickmy7@gmail.com>
2018-10-16 02:44:46 +0900

    주석 추가 및 print 주석처리

--- files n sql/morse_2.py
+++ files n sql/morse_2.py
@@ -9,25 +9,25 @@ import re
 
 #webpage
 # url = 'https://librewiki.net/wiki/%EB%AA%A8%EC%8A%A4_%EB%B6%80%ED%98%B8' #주소1
-url = 'http://codingdojang.com/scode/469?langby=python#answer-filter-area'
+url = 'http://codingdojang.com/scode/469?langby=python#answer-filter-area' #주소2
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'}
 res = requests.get(url, headers=headers)
-print(res)
+# print(res)
 
 #info
 data = res.text
-print(data)
+# print(data)
 
 #parsing
 soup = BeautifulSoup(data, 'lxml')
-
-print(soup.text)
+# print(soup.text)
 
 # storage to save signals
 signals = []
 
+# collect text data
 # for t in soup.select('.wikitable tr td'): #주소1
-for t in soup.select('div.markdown_area.answer-content > table > tbody tr td'): #주소1
+for t in soup.select('div.markdown_area.answer-content > table > tbody tr td'): #주소2
     pattern = re.compile(r'\s+')
     t_data = re.sub(pattern,'', t.text)
     print(t_data)
@@ -37,7 +37,7 @@ for t in soup.select('div.markdown_area.answer-content > table > tbody tr td'): 
 # #string data into a Dictionary list
 allData = OrderedDict()
 # for i in range(int(len(soup.select('.wikitable tr td'))/2)): #주소1
-for i in range(int(len(soup.select('div.markdown_area.answer-content > table tr td'))/2)): #주소1
+for i in range(int(len(soup.select('div.markdown_area.answer-content > table tr td'))/2)): #주소2
     #dictionary 형태로 저장.
     allData[signals[2*i+1]] = signals[2*i]
     # allData[signals[2*i]] = signals[2*i+1]
@@ -52,12 +52,13 @@ wordslists = []
 
 #데이터 분석
 morse = '.... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--'
+# 빈 칸이 1칸일 경우 구분자로 인식되지만 2칸일 경우 ''의 형태로 저장된다.
 wordslists = morse.split(" ")
 
 result = ""
-for list in wordslists:
-    if list == '':
+for wordlist in wordslists:
+    if wordlist == '':
         result += ' '
     else:
-        result += allData[list]
+        result += allData[wordlist]
 print(result)
